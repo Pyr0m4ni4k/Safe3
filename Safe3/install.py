94cf583ce24fbf28ff05bb9748dbb8a9de495224
Pyromaniak <85025717+Pyr0m4ni4k@users.noreply.github.com>
2022-01-18 13:32:13 +0100

    Add files via upload

---
+++ Safe3/install.py
@@ -0,0 +1,9 @@
+import os
+
+os.system("mkdir files")
+tmp = open("tmp.txt", "w") 
+ctxt = open("countfile.txt", "w") 
+ltxt = open("loginfile.txt", "w") 
+npsw = open("namepswd.txt", "w") 
+rwx = open("rxwts98.txt ", "w") 
+os.system("chmod 777 -R *")
